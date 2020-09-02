2bd3ab89494cc0a4629078366f98c215a93e7789
Clinton Hastings <60298559+MegaClin10@users.noreply.github.com>
2020-09-01 21:14:21 -0500

    full functionality, exporting to csv every 5 min

--- dataExtractor.py
+++ dataExtractor.py
@@ -7,45 +7,50 @@ BEFORE RUNNING:
 2. Install the Python client library for Google APIs by running
    `pip install --upgrade google-api-python-client`
 """
-from pprint import pprint
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from googleapiclient import discovery
-
 import string
+import time
+from itertools import count
+from pprint import pprint
+from datetime import datetime
+import gspread
+from googleapiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
 
-# TODO: Change placeholder below to generate authentication credentials. See
-# https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
-#
-# Authorize using one of the following scopes:
-#     'https://www.googleapis.com/auth/drive'
-#     'https://www.googleapis.com/auth/drive.file'
-#     'https://www.googleapis.com/auth/drive.readonly'
-#     'https://www.googleapis.com/auth/spreadsheets'
-#     'https://www.googleapis.com/auth/spreadsheets.readonly'
-credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json")
 
-service = discovery.build('sheets', 'v4', credentials=credentials)
+def getPatronCount():
+   credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json")
 
-# The ID of the spreadsheet to retrieve data from.
-spreadsheet_id = '1AV5IqNhVaiFLq9hEDezLs4JYm4OcepfN1uux-E0NzdY'  # TODO: Update placeholder value.
+   service = discovery.build('sheets', 'v4', credentials=credentials)
 
-# The A1 notation of the values to retrieve.
-range_ = 'A13:A13'  # TODO: Update placeholder value.
+   # The ID of the spreadsheet to retrieve data from.
+   spreadsheet_id = '1AV5IqNhVaiFLq9hEDezLs4JYm4OcepfN1uux-E0NzdY'  # TODO: Update placeholder value.
 
-# How values should be represented in the output.
-# The default render option is ValueRenderOption.FORMATTED_VALUE.
-value_render_option = 'FORMATTED_VALUE'  # TODO: Update placeholder value.
+   # The A1 notation of the values to retrieve.
+   range_ = 'A13:A13'  # TODO: Update placeholder value.
 
-# How dates, times, and durations should be represented in the output.
-# This is ignored if value_render_option is
-# FORMATTED_VALUE.
-# The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-date_time_render_option = 'SERIAL_NUMBER'  # TODO: Update placeholder value.
+   # How values should be represented in the output.
+   # The default render option is ValueRenderOption.FORMATTED_VALUE.
+   value_render_option = 'FORMATTED_VALUE'  # TODO: Update placeholder value.
 
-patronRequest = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
-response = patronRequest.execute()
+   # How dates, times, and durations should be represented in the output.
+   # This is ignored if value_render_option is
+   # FORMATTED_VALUE.
+   # The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+   date_time_render_option = 'SERIAL_NUMBER'  # TODO: Update placeholder value.
 
-# TODO: Change code below to process the `response` dict:
-currentPatronCount = str(response.get('values')).strip('[]\'')
-print(currentPatronCount)
+   patronRequest = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
+   response = patronRequest.execute()
+
+   # TODO: Change code below to process the `response` dict:
+   currentPatronCount = str(response.get('values')).strip('[]\'')
+   print(currentPatronCount)
+   return currentPatronCount
+
+
+
+#main
+f = open('../../recCount/recCount.csv', 'w')
+while True:
+   f.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "," + getPatronCount() + "\n") 
+   time.sleep(300)
+f.close()
